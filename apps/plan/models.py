2a89452f6545a2abdc8a10c1b914fe0ca55db83b
lhr520czx <1090007917@qq.com>
2017-06-21 12:12:00 +0800

    增加 宣传计划申请所有

--- apps/plan/models.py
+++ apps/plan/models.py
@@ -9,10 +9,14 @@ from xuanchuan.models import Opinion,DraftBase
 class PropagatePlan(models.Model):
     main = models.OneToOneField(DraftBase, verbose_name='基础信息')
     plan_timestyle = models.CharField(max_length=10, verbose_name='计划时间类别', default='')
+    team = models.CharField(max_length=10 , default='', verbose_name='小组名称')
+    plan_style = models.CharField(max_length=10 , default='', verbose_name='计划类别')
     blame_person = models.CharField(max_length=10, verbose_name='负责人')
-    plan_time = models.CharField(default='', verbose_name='计划时间', max_length=20)
+    member = models.CharField(max_length=10 , default='', verbose_name='成员')
+    start_time = models.CharField(default='', verbose_name='计划开始时间', max_length=20)
+    end_time = models.CharField(default='', verbose_name='计划结束时间', max_length=20)
+    content = models.TextField(default='', verbose_name='正文内容')
     remark = models.CharField(max_length=200, verbose_name='备注')
-    office = models.ManyToManyField(Office, verbose_name='小组')
     file = models.FileField(upload_to='xc/files/%Y/%m', verbose_name='附件', max_length=100,
                             null=True, blank=True, default='')
     opinion = models.ForeignKey(Opinion, verbose_name='领导意见', null=True, blank=True, default='')
@@ -26,13 +30,13 @@ class PropagatePlan(models.Model):
 
 
 class WorkTarget(models.Model):
-    target_user = models.ForeignKey(UserProfile, verbose_name='填指标者', related_name='target_user')
-    add_time = models.DateTimeField(default='', verbose_name='申请时间')
-    work_target = models.CharField(max_length=200, verbose_name='工作指标')
+    target_user = models.CharField(default='', verbose_name='负责人', max_length=10)
+    name = models.CharField(default='', verbose_name='项目名称', max_length=20)
+    content = models.CharField(default='', verbose_name='具体内容', max_length=20)
     finish_time = models.CharField(default='', verbose_name='完成时间', max_length=20)
-    finish_number = models.IntegerField(default='', verbose_name='完成次数')
-    content = models.TextField(max_length=500, verbose_name='内容')
+    schedule = models.CharField(max_length=10, verbose_name='进度', default='')
+    complete_status = models.TextField(default='', verbose_name='完成情况')
+    is_finish = models.BooleanField(verbose_name='是否完成', default=False)
     file = models.FileField(upload_to='xc/files/%Y/%m', verbose_name='附件', max_length=100,
                             null=True, blank=True, default='')
-    remark = models.CharField(max_length=200, verbose_name='备注')
     propagate_plan = models.ForeignKey(PropagatePlan , verbose_name='计划表')
